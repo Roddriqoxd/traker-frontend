c3680621c7957a406a9eccf008b2822729758fd6
Rodrigo Lopez <roddriqoxd@gmail.com>
2024-07-09 23:36:02 -0400

    create modules and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthModule } from "./auth/auth.module";
+import { LoginComponent } from "./auth/login/login.component";
 
 @Component({
   selector: 'app-root',
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormControl,FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+
+
+
+    email= new FormControl('', [Validators.required, Validators.email])
+    password= new FormControl('', [Validators.required])
+}
